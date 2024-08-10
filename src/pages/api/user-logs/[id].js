678480823ef2c1b04d0b17fadfd959b061311adf
Aditya Cahyo <aditydcp@gmail.com>
2024-08-11 03:40:34 +0700

    add user log api

---
+++ src/pages/api/user-logs/[id].js
@@ -0,0 +1,24 @@
+import { getById } from "../../../utils/services/queries"
+import { table } from "../../../utils/types/user-logs";
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  if (req.method === 'GET') {
+    const { data, error } = await getById(table, id);
+    if (data) {
+      res.status(200).json({
+        message: `GET user log with id ${id}`,
+        data: data,
+        error: error
+      })
+    } else {
+      res.status(404).json({
+        message: `User Log with id ${id} not found`,
+        error: error
+      })
+    }
+  } else {
+    res.status(405).end('Method Not Allowed')
+  }
+}
