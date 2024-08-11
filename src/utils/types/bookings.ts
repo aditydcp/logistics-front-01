3b49986c310ea6cf1cf7a75f139c099ee8a8f820
Aditya Cahyo <aditydcp@gmail.com>
2024-08-11 12:42:43 +0700

    Update bookings.ts

--- src/utils/types/bookings.ts
+++ src/utils/types/bookings.ts
@@ -5,14 +5,14 @@ export const table = 'bookings';
 export interface Booking {
   id: number;
   user_id: number;
-  exporter_id: number;
-  importer_id: number;
-  flight_id: number;
-  category: number;
-  packaging: number;
-  quantity: number;
-  dimension: number;
-  weight: number;
+  exporter_id: number | null;
+  importer_id: number | null;
+  flight_id: number | null;
+  category_id: number | null;
+  packaging_id: number | null;
+  quantity: number | null;
+  dimension: number | null;
+  weight: number | null;
   status: number;
   created_at: string;
   updated_at: string;
@@ -24,8 +24,8 @@ export const bookingSchema = {
   exporter_id: 'integer reference exporters(id)',
   importer_id: 'integer reference importers(id)',
   flight_id: 'integer reference flights(id)',
-  category: 'integer reference categories(id)',
-  packaging: 'integer reference packagings(id)',
+  category_id: 'integer reference categories(id)',
+  packaging_id: 'integer reference packagings(id)',
   quantity: 'integer',
   dimension: 'real',
   weight: 'real',
@@ -52,12 +52,12 @@ export function isValidBooking(data: any): data is Booking {
     typeof data.exporter_id === 'number' &&
     typeof data.importer_id === 'number' &&
     typeof data.flight_id === 'number' &&
-    typeof data.category === 'number' &&
-    typeof data.packaging === 'number' &&
+    typeof data.category_id === 'number' &&
+    typeof data.packaging_id === 'number' &&
     typeof data.quantity === 'number' &&
     typeof data.dimension === 'number' &&
     typeof data.weight === 'number' &&
     typeof data.status === 'number' &&
-    Object.keys(data).every(key => ['user_id', 'exporter_id', 'importer_id', 'flight_id', 'category', 'packaging', 'quantity', 'dimension', 'weight', 'status'].includes(key))
+    Object.keys(data).every(key => ['user_id', 'exporter_id', 'importer_id', 'flight_id', 'category_id', 'packaging_id', 'quantity', 'dimension', 'weight', 'status'].includes(key))
   )
 }
